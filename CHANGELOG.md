a555f6960068547486531e1d7babb9bb5c43791e
Pierre-Yves  Orban <orban.pierreyves@gmail.com>
2017-05-12 12:37:06 +0200

    add more methods and bump to 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,13 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [0.2.0] - 2017-05-12
+### Added
+- Method `link_contact_to_company`
+- Methods for Products (add, update, delete, get one, get all)
+- Methods for Invoice (add, update payment status, get PDF, get one, get all)
+- Update `get_company` to accept an Hash instead of an id as parameter
+
 ## [0.1.1] - 2017-05-12
 ### Changed
 - Mark `json` dependency version as `>= 1`
--- lib/teamleader/api.rb
+++ lib/teamleader/api.rb
@@ -41,8 +41,9 @@ module Teamleader
       request "/addCompany.php", params
     end
 
-    def get_company(id)
-      request "/getCompany.php", {:company_id => id}
+    def get_company(params={})
+      raise "company_id is required" if params[:company_id].nil?
+      request "/getCompany.php", params
     end
 
     def get_companies(params={})
@@ -52,20 +53,65 @@ module Teamleader
     end
 
     def add_invoice(params={})
-      raise "contact_or_company" if params[:contact_or_company].nil?
-      raise "contact_or_company_id" if params[:contact_or_company_id].nil?
-      raise "sys_department_id" if params[:sys_department_id].nil?
+      raise "contact_or_company is required" if params[:contact_or_company].nil?
+      raise "contact_or_company_id is required" if params[:contact_or_company_id].nil?
+      raise "sys_department_id is required" if params[:sys_department_id].nil?
       request "/addInvoice.php", params    
     end
 
-    def get_invoice(id)
-      request "/getInvoice.php", {:invoice_id => id}
+    def update_invoice_payment_status(params={})
+      raise "invoice_id is required" if params[:invoice_id].nil?
+      raise "status is required" if params[:status].nil?
+      raise "status must be 'paid' or 'not_paid'" unless ['paid', 'not_paid'].include?(params[:status])
+      request "/setInvoicePaymentStatus.php", params    
+    end
+
+    def get_invoice(params={})
+      raise "invoice_id is required" if params[:invoice_id].nil?
+      request "/getInvoice.php", params
+    end
+
+    def download_invoice_pdf(params={})
+      raise "invoice_id is required" if params[:invoice_id].nil?
+      request "/downloadInvoicePDF.php", params
     end
 
     def get_invoices(params={})
       raise "date_from is required" if params[:date_from].nil?
       raise "date_to is required" if params[:date_to].nil?
       request "/getInvoices.php", params
+    end
+
+    def add_product(params={})
+      raise "name is required" if params[:name].nil?
+      raise "price is required" if params[:price].nil?
+      request "/addProduct.php", params
+    end
+
+    def update_product(params={})
+      raise "product_id is required" if params[:product_id].nil?
+      request "/updateProduct.php", params
+    end
+
+    def delete_product(params={})
+      raise "product_id is required" if params[:product_id].nil?
+      request "/deleteProduct.php", params
+    end
+
+    def get_product(params={})
+      raise "product_id is required" if params[:product_id].nil?
+      request "/getProduct.php", params
+    end
+
+    def get_product(params={})
+      raise "product_id is required" if params[:product_id].nil?
+      request "/getProduct.php", params
+    end
+
+    def get_products(params={})
+      raise "amount is required" if params[:amount].nil?
+      raise "pageno is required" if params[:pageno].nil?
+      request "/getProducts.php", params
     end
 
     private
