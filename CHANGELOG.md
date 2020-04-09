08ca304e1f40dee1919b5601a1a1ea3ee48e782e
Pierre-Yves Orban <pyo@javry.com>
2020-04-09 14:54:24 +0200

    get_all_subscriptions method accepts optional parameters

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [0.16.1] - 2020-04-09
+- `get_all_subscriptions` method accepts optional parameters (like `selected_customfields` or `searchby`)
+
 ## [0.16.0] - 2019-05-10
 - New method related to custom fields: `get_custom_fields`, `get_custom_field_info`, `add_custom_field_option`
 
--- lib/teamleader/extended_api.rb
+++ lib/teamleader/extended_api.rb
@@ -3,11 +3,11 @@ module Teamleader
   # The Api class is a 1-1 mapping with Teamleader API v1
   # The ExtendedApi adds some utility methods
   class ExtendedApi < Api
-    def get_all_subscriptions
+    def get_all_subscriptions(**args)
       subscriptions = []
       pageno = 0
       loop do
-        results = self.get_subscriptions(pageno: pageno, amount: 100)
+        results = self.get_subscriptions((args || {}).merge({pageno: pageno, amount: 100}))
         subscriptions.concat(results)
         pageno += 1
         break if results.length == 0
